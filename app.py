30070bff30ab27c036e3890ef7ef2bb9c8c970aa
Ahmet Dizdar <117534684+ahmetdzdrr@users.noreply.github.com>
2023-09-14 22:15:31 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import streamlit as st
 from main import *
 import os
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title("Airbnb Analysis")
 st.markdown("---")
 
 option = st.selectbox("Choose option", options=["None", "Pull Data", "Show Data"])
 
-button = st.button("Apply")
+if option == 'Pull Data':
+    request()
 
-if button:
-    if option=='Pull Data':
-        request()
+if option == 'Show Data':
+    if os.path.exists("airbnb_data.csv"):
+        df = pd.read_csv("airbnb_data.csv", index_col=None)
+        analysis(df)
+        st.markdown("---")
+        pie_visualization(df)
+        st.markdown("---")
+        barplot_visualization(df)
+    else:
+        st.warning("Please pull the data first using the 'Pull Data' checkbox.", icon="⚠️")
 
-    if option=='Show Data':
-        if os.path.exists("airbnb_data.csv"):
-            df = pd.read_csv("airbnb_data.csv", index_col=None)
-            analysis(df)
-        else:
-            st.warning("Please pull the data first using the 'Pull Data' checkbox.")
 
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 import streamlit as st
 import configparser
 import warnings
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
 warnings.simplefilter("ignore")
 
 def request():
@@ -61,3 +64,32 @@ def analysis(dataframe):
 
     pd.set_option("display.max_columns", 500)
     st.write(dataframe)
+    return dataframe
+
+def pie_visualization(dataframe):
+    columns = [col for col in dataframe.columns if dataframe[col].nunique() < 6]
+    column_option = st.selectbox("Choose a feature for Pie Chart:", ['None'] + columns)
+    if column_option != 'None':
+        target_counts = dataframe[column_option].value_counts()
+
+        colors = [plt.cm.Paired(random.choice(range(12))) for _ in range(len(target_counts))]
+
+        wedgeprops = {'linewidth': 2, 'edgecolor': 'white'}
+        fig, ax = plt.subplots()
+        ax.pie(target_counts, labels=target_counts.index, autopct='%1.2f%%',
+               startangle=90, wedgeprops=wedgeprops, colors=colors)
+        ax.axis('equal')
+        ax.set_title(f"{column_option} Pie Chart")
+        st.pyplot(fig)
+
+
+def barplot_visualization(dataframe):
+    columns = [col for col in dataframe.columns if dataframe[col].nunique() > 6]
+    columns_option = st.selectbox("Choose a feature for Bar Chart:", ["None"] + columns)
+
+    if columns_option != 'None':
+        plt.figure(figsize=(12, 6))
+        plt.title(f"{columns_option} Bar Plot")
+        sns.set(font_scale=1.5)
+        sns.countplot(data=dataframe, x=columns_option)
+        st.pyplot()
